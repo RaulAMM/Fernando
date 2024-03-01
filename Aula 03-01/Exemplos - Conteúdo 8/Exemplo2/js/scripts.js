b577dd4963c18810faece5edf362c25d25c4a364
Fernando Vieira Duarte <79167486+fernandoduarte75@users.noreply.github.com>
2024-03-01 11:38:17 -0300

    Aula 01/03/2024

---
+++ Aula 03-01/Exemplos - Conteúdo 8/Exemplo2/js/scripts.js
@@ -0,0 +1,10 @@
+let number1, number2, sum;
+
+document.write('Número 1 = ' + number1);
+
+number1 = parseInt(prompt("Digite um número"));
+number2 = parseInt(prompt("Digite outro número"));
+
+sum = number1 + number2;
+
+document.write('Soma = ' + sum);
